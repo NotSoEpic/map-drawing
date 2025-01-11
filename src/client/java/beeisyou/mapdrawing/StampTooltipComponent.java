3ed8674f68dc546ea5cf063250b451d0970c3c17
NotSoEpic <legotomsmith@gmail.com>
2025-01-11 11:16:02 +0800

    betterer stamp item (item texture by LlamaMan)

--- src/client/java/beeisyou/mapdrawing/StampTooltipComponent.java
+++ src/client/java/beeisyou/mapdrawing/StampTooltipComponent.java
@@ -1,34 +1,41 @@
 package beeisyou.mapdrawing;
 
+import beeisyou.mapdrawing.color.ColorPaletteManager;
+import beeisyou.mapdrawing.rendering.MapDrawingRenderLayers;
+import beeisyou.mapdrawing.stampitem.StampTextureTooltipData;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.util.Identifier;
 import org.joml.Matrix4f;
 
-public class StampTooltipComponent implements TooltipComponent {
-    public final Identifier texture;
-    public StampTooltipComponent(Identifier texture) {
-        this.texture = texture;
-    }
+public record StampTooltipComponent(Identifier texture) implements TooltipComponent {
     @Override
     public int getHeight(TextRenderer textRenderer) {
-        return 16;
+        return 32 + 2;
     }
 
     @Override
     public int getWidth(TextRenderer textRenderer) {
-        return 16;
+        return 32;
     }
 
     @Override
     public void drawText(TextRenderer textRenderer, int x, int y, Matrix4f matrix, VertexConsumerProvider.Immediate vertexConsumers) {
-        VertexConsumer consumer = vertexConsumers.getBuffer(RenderLayer.getGuiTextured(texture));
+        MapDrawingClient.palette = ColorPaletteManager.get(MapDrawing.id("default"));
+        VertexConsumer consumer = vertexConsumers.getBuffer(MapDrawingRenderLayers.getPaletteSwap(texture));
         consumer.vertex(matrix, x, y, 0).texture(0, 0).color(-1);
-        consumer.vertex(matrix, x, y + 16, 0).texture(0, 1).color(-1);
-        consumer.vertex(matrix, x + 16, y + 16, 0).texture(1, 1).color(-1);
-        consumer.vertex(matrix, x + 16, y, 0).texture(1, 0).color(-1);
+        consumer.vertex(matrix, x, y + 32, 0).texture(0, 1).color(-1);
+        consumer.vertex(matrix, x + 32, y + 32, 0).texture(1, 1).color(-1);
+        consumer.vertex(matrix, x + 32, y, 0).texture(1, 0).color(-1);
+    }
+
+    public static String fromPathShorthand(String path) {
+        return "textures/stamp/" + path + ".png";
+    }
+
+    public static StampTooltipComponent fromComponent(StampTextureTooltipData component) {
+        return new StampTooltipComponent(component.texture().withPath(StampTooltipComponent::fromPathShorthand));
     }
 }
--- src/client/java/beeisyou/mapdrawing/datagen/LangGenerator.java
+++ src/client/java/beeisyou/mapdrawing/datagen/LangGenerator.java
@@ -1,9 +1,11 @@
 package beeisyou.mapdrawing.datagen;
 
 import beeisyou.mapdrawing.AllItems;
+import beeisyou.mapdrawing.MapDrawing;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.Identifier;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -15,5 +17,12 @@ public class LangGenerator extends FabricLanguageProvider {
     @Override
     public void generateTranslations(RegistryWrapper.WrapperLookup registryLookup, TranslationBuilder translationBuilder) {
         translationBuilder.add(AllItems.STAMP, "Stamp");
+
+        stamp(translationBuilder, Identifier.ofVanilla("missingno"), "Unusual Pattern");
+        stamp(translationBuilder, MapDrawing.id("x_marks_the_spot"), "X Marks the Spot");
+    }
+
+    private void stamp(TranslationBuilder translationBuilder, Identifier stamp, String value) {
+        translationBuilder.add(stamp.toTranslationKey("stamp"), value);
     }
 }
